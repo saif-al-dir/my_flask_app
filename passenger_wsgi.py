2cce8080eee8c8f6f84c9c8a3a98539903c227de
Saif Al-Diresee <saif.aldiresee@gmail.com>
2026-08-22 20:17:26 +0200

    Fix: Used site.addsitedir to properly load venv

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,19 +1,15 @@
 import sys
 import os
 
-# 1. Get the directory where this file is located
 PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
+INTERP = os.path.join(PROJECT_DIR, 'venv', 'bin', 'python')
 
-# 2. Manually add the venv's site-packages to sys.path (Now using python3.12!)
-VENV_SITE_PACKAGES = os.path.join(PROJECT_DIR, 'venv', 'lib', 'python3.12', 'site-packages')
-if VENV_SITE_PACKAGES not in sys.path:
-    sys.path.insert(0, VENV_SITE_PACKAGES)
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
 
-# 3. Add the project directory to sys.path so it finds the 'app' folder
 if PROJECT_DIR not in sys.path:
     sys.path.insert(0, PROJECT_DIR)
 
-# 4. Now import and create your Flask app
 from app import create_app
 application = create_app()
 
