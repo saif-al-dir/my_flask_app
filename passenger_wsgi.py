0939b674382295310d9acc8e32649c72a2769536
Saif Al-Diresee <saif.aldiresee@gmail.com>
2026-08-22 22:37:51 +0200

    Fix: Restored os.execl venv trick

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -4,6 +4,12 @@ import os
 PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
 INTERP = os.path.join(PROJECT_DIR, 'venv', 'bin', 'python')
 
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
+
+if PROJECT_DIR not in sys.path:
+    sys.path.insert(0, PROJECT_DIR)
+
 from app import create_app
 application = create_app()
 
